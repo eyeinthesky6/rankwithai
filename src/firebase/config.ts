e831d5bc13c93f195e699b702b7ba951b9f3bc13
Jai Thakur <thakur.jai@gmail.com>
2026-03-03 18:41:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2888119241-9c0e0",
+  "appId": "1:313939026397:web:ead4272e281e717af3d634",
+  "apiKey": "AIzaSyChG4nYAberwYlH0trSsX_Faotv3gW8u1M",
+  "authDomain": "studio-2888119241-9c0e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "313939026397"
+};
